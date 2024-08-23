66e2493e65ca22c934ed3173bc06826ff0688616
MountVesuvius <JudeALakkis@gmail.com>
2024-08-23 23:16:19 +1000

    ✨ JWT Service in a functional state
    the service can generate and validate tokens as expected

--- services/jwt_service.go
+++ services/jwt_service.go
@@ -11,34 +11,60 @@ import (
 
 type (
     JWTService interface {
-        GenerateToken(userId string, role string) (string, error) 
+        GenerateAccessToken(userId string, role string) (string, error) 
+        GenerateRefreshToken() (string, error)
         ValidateToken(token string) (*jwt.Token, error)
     }
 
     jwtService struct {
-        secretKey string
+        secretKey []byte
         issuer string
     }
 )
 
 func NewJWTService() JWTService {
     return &jwtService{
-        secretKey: os.Getenv("AUTH_SECRET"),
+        secretKey: []byte(os.Getenv("AUTH_SECRET")),
         issuer: os.Getenv("ISSUER"),
     }
 }
 
-func (j *jwtService) GenerateToken(userId string, role string) (string, error) {
-    token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-        "user": userId,
-        "role": role,
-        "exp": time.Now().Add(time.Hour * 1).Unix(),
+
+func (j *jwtService) GenerateAccessToken(userId string, role string) (string, error) {
+    // Create access claims
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims {
+        "sub": userId,
+        "exp": jwt.NewNumericDate(time.Now().Add(time.Minute * 15)),
+        "iat": jwt.NewNumericDate(time.Now()),
+        "iss": "this-backend-needs-an-iss-id",
+        "role": "user-role",
+        "type": "access",
     })
+    fmt.Println("Access Token Generation", token)
+
+    // Sign the access token
     tokenString, err := token.SignedString(j.secretKey)
     if err != nil {
         return "", err
     }
+    fmt.Println("Signed Access Token Generation", tokenString)
+    return tokenString, nil
+}
 
+func (j *jwtService) GenerateRefreshToken() (string, error) {
+    // Create refresh claims (last one day)
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims {
+        "exp": jwt.NewNumericDate(time.Now().Add(time.Hour * 24)),
+        "iat": jwt.NewNumericDate(time.Now()),
+        "iss": "this-backend-needs-an-iss-id",
+        "type": "refresh",
+    })
+
+    // Sign the refresh token
+    tokenString, err := token.SignedString(j.secretKey)
+    if err != nil {
+        return "", err
+    }
     return tokenString, nil
 }
 
